9cc02df67bfcc6adf9d3470b02d8f121f944a0c3
Travis Gayle <TravisGayle@traviss-air.dhcp.nd.edu>
2017-03-31 02:31:35 -0400

    Initial Commit

---
+++ ScreenReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenReader
+//
+//  Created by Travis Gayle on 3/31/17.
+//  Copyright © 2017 TravisGayle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
